febad9f056726e1998ec341a2cbf757cbdea697e
Stephen Hamilton <stephenshamilton@gmail.com>
2016-05-01 16:52:20 -0400

    initial weather tcp server

--- README.md
+++ README.md
@@ -3,4 +3,45 @@ Program to read weewx and output peet format for Xastir or other apps
 Weewx stores data in MySQL, and this script reads the current weather data, outputs it in Peet format for Xastir to read and utilize for APRS weather data.
 
 
+Arne format
+  /* Check that user wants Arne-style broadcasts? */
+  /* Using the port number as a flag */
+  if (!setup_arne_udp_port && !server_arne.port) return 0 ;
+
+  sprintf(buffer,
+    "%2.1f %2.1f %2.1f %2.1f %2.1f %2.1f %d %d %3.3f %3.3f %3.3f %3.3f\r\n",
+    weather_primary_T(NULL),
+    wd->Tmax /*max_tempC*/,
+    wd->Tmin /*min_tempC*/,
+    wd->anem_mps,
+    wd->anem_gust /*peak_speedMS*/,
+    wd->anem_speed_max * 0.447040972 /*max_speedMS*/,
+    wd->vane_bearing - 1 /*current_dir*/,
+    wd->vane_mode /*max_dir*/,
+    wd->rain_rate /*rain_rateI*/,
+    (wd->rain >= 0.0F) ? wd->rain : 0.0,
+    (wd->rain >= 0.0F) ? 0.01F * (float)
+                                 (wd->rain_count - wd->rain_offset[6]) :
+                         0.0F,
+    (wd->rain >= 0.0F) ? 0.01F * (float)
+                                 (wd->rain_count - wd->rain_offset[7]) :
+                         0.0F);
+ /* Perhaps we have more data? */
+
+    if ((strlen(buffer) > nread) &&
+        (9 == sscanf(&buffer[nread],
+         "%f %f %f %f %f %f %f %f %d",
+         &wd->RH[0],
+         &RH_max,
+         &RH_min,
+         &wd->barom[0],
+         &barom_max,
+         &barom_min,
+         &barom_rate,
+         &wd->rain_rate,
+         &check)))
+    {
+
+
+
 
--- wxreader.py
+++ wxreader.py
@@ -1,4 +1,77 @@
-#Program to read weewx and output peet format for Xastir or other apps
+#Program to read weewx and output Arne Henrikson format for Xastir or other apps
 #Written by Stephen Hamilton
 
+import socket
+import sys
+import time
+import mysql.connector
 
+def main():
+    #Xastir client ip address
+    xastir_addr = ('192.168.1.1', 8888)
+    # Create a TCP/IP socket
+    #sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    cnx = mysql.connector.connect(user='weewx', database='weewx', host='ipa', password='xxxxxx')
+    cursor = cnx.cursor()
+    # Bind the socket to the port
+    server_address = ('localhost', 8888)
+    print >>sys.stderr, 'starting up on %s port %s' % server_address
+    sock.bind(server_address)
+    sock.listen(1)
+    connection, client_address = sock.accept()
+    data = connection.recv(4096)
+    print >>sys.stderr, 'received "%s"' % data
+    while True:
+        print "Sending output"
+        data = getweatherpacket(cursor)
+        
+        print ("Waiting to receive")
+        #data, server = sock.recvfrom(4096)
+        
+        print >>sys.stderr, 'Sending "%s"' % data
+        connection.sendall(data)
+        #sent = sock.sendto(data)
+        time.sleep(5) 
+        
+
+def getweatherpacket(cursor):
+    #MySQL String
+    sqlstring = "select outTemp, windSpeed, windGust, windDir, rainRate, rain, outHumidity, barometer, pressure from archive order by dateTime desc limit 1;"
+    #Replace with mysql command to get latest weather
+    #Format:
+    #temp(C), maxT(C), minT(c), wd->anem_mps, wd->anem_gust /*peak_speedMS*/,
+    #wd->anem_speed_max * 0.447040972 /*max_speedMS*/,
+    #wd->vane_bearing - 1 /*current_dir*/,
+    #wd->vane_mode /*max_dir*/,
+    #wd->rain_rate /*rain_rateI*/,                                   #H
+    #            T    MT   mT    spd   #b                              #H
+    swxstring = "17.7 38.7 19.7 .44704 .70 30.0 4 5 30.0 40.0 50.0 60.0 24 2 3 4 5 6 7 8 9 10\r\n"
+    #print("Sample\n")
+    wxstring = ""
+    #print swxstring
+    cursor.execute(sqlstring)
+    row = cursor.fetchone()
+    #print (row)
+    temp = (row[0]-32)*5/9
+    wxstring = '{:03.1f}'.format(temp) #outside temp
+    wxstring = wxstring + " " + '{:03.1f}'.format(temp) #max temp
+    wxstring = wxstring + " " + '{:03.1f}'.format(temp) #min temp
+
+    wxstring = wxstring + " " + '{:03.1f}'.format(row[1]*1.609/3.66) #speed
+    wxstring = wxstring + " " + '{:03.1f}'.format(row[2]*1.609/(3.66)) #gust
+    if (row[3] == None):
+        winddir = 0
+    else:
+        winddir = int((row[3]/22.5)+.5)
+    wxstring = wxstring + " 0.0 " + str(winddir) #maxspeed (unused), bearing
+    wxstring = wxstring + " 1 " + '{:03.3f}'.format(row[4]) #max_dir (unused), rainrate
+    wxstring = wxstring + " " + '{:02.2f}'.format(row[5]) #rain
+    wxstring = wxstring + " 6.0 5.0 " + '{:03.3f}'.format(row[6]) #outHumidity
+    wxstring = wxstring + " 1.0 1.0 " + '{:03.3f}'.format(row[7]) #RHmax (unused), rhmin(unused), barometer    print ("sending %s" % wxstring)
+    wxstring = wxstring + "1.0 1.0 1.0 1.0 1.0 1.0 1.0\r\n"
+    return wxstring
+
+
+if __name__ == "__main__":
+    main()
